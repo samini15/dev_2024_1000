cc34389dff3dbd8043ef2d3c445e76fabc43736a
Shayan <shayan.amini76@gmail.com>
2024-12-12 14:19:17 +0100

    Basic setup + Splash screen

---
+++ app/src/main/java/com/example/dev_2024_1000/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.dev_2024_1000
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
+import com.example.dev_2024_1000.ui.theme.Dev_2024_1000Theme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        var showSplashScreen by mutableStateOf(true)
+        splashScreen.setKeepOnScreenCondition { showSplashScreen }
+        lifecycleScope.launch {
+            delay(2500) // 2.5 seconds
+            showSplashScreen = false
+            withContext(Dispatchers.Main) {
+                splashScreen.setKeepOnScreenCondition { showSplashScreen }
+                setContent {
+                    Dev_2024_1000Theme {
+                        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                            Greeting(
+                                name = "Android",
+                                modifier = Modifier.padding(innerPadding)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Dev_2024_1000Theme {
+        Greeting("Android")
+    }
+}
